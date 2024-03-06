00ddf3375acde02e0092962abd95dfaa248de4f0
HFVostok <hiago.fernandor7@gmail.com>
2024-03-05 21:58:57 -0300

    2024.03.05.01

--- exemplo_widget/lib/layout_basico.dart
+++ exemplo_widget/lib/layout_basico.dart
@@ -8,49 +8,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Container Layout',
+      title: 'Layout Básico ', // Define o título do aplicativo
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Container Layout'),
+          title: Text('Layout Básico'), // Define o título da barra de aplicativos
         ),
         body: Center(
           child: Container(
-            width: 200,
-            height: 200,
-            color: Colors.blue,
+            width: 500, // Define a largura do Container
+            height: 500, // Define a altura do Container
+            color: Colors.blue, // Define a cor de fundo do Container
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center, // Alinha os widgets filhos verticalmente ao centro
               children: <Widget>[
-                Text('Widget 1',
+                Text('Filho Widget 1', // Texto do primeiro widget
                   style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
+                    color: Colors.white, // Define a cor do texto
+                    fontSize: 40, // Define o tamanho da fonte
                   ),
                 ),
-                SizedBox(height: 20),
+                //====================================================
+                SizedBox(height: 50), // Espaço vertical entre os widgets
                 Container(
-                  width: 100,
-                  height: 50,
-                  color: Colors.green,
+                  width: 100, // Define a largura do Container 2
+                  height: 50, // Define a altura do Container 2
+                  color: Colors.green, // Define a cor de fundo do Container 2
                   child: Center(
-                    child: Text('Widget 2',
+                    child: Text('Filho Widget 2', // Texto do segundo widget
                       style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
+                        color: Colors.white, // Define a cor do texto
+                        fontSize: 16, // Define o tamanho da fonte
                       ),
                     ),
                   ),
                 ),
-                SizedBox(height: 20),
+                //=======================================================
+                SizedBox(height: 20), // Espaço vertical entre os widgets
                 Container(
-                  width: 150,
-                  height: 100,
-                  color: Colors.orange,
+                  width: 140, // Define a largura do Container 3
+                  height: 120, // Define a altura do Container 3
+                  color: Colors.orange, // Define a cor de fundo do Container 3
                   child: Center(
-                    child: Text('Widget 3',
+                    child: Text('Filho Widget 3', // Texto do terceiro widget
                       style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
+                        color: Colors.white, // Define a cor do texto
+                        fontSize: 23, // Define o tamanho da fonte
                       ),
                     ),
                   ),
